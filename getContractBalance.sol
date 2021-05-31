6664fe0bceeee0b2f8ebec0239624a8ea894181c
Nico <73706148+Nicofru@users.noreply.github.com>
2021-06-01 00:12:00 +0200

    Update getContractBalance.sol

--- getContractBalance.sol
+++ getContractBalance.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.6.11;
 
-contract Whitelist {
+contract getContractBalance {
     
     function getBalance() public view returns (uint){
         return address(0xb2f55315C465297A5926795d5Bb94f985209398E).balance;
