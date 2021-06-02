06943ea76f08acaf665aa20ac2fe9628bf59520b
Nicofru <n.fruneau@gmail.com>
2021-06-02 17:41:04 +0200

    up

--- whitelist.sol
+++ whitelist.sol
@@ -1,27 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.6.11;
+pragma solidity ^0.8.0;
 
-contract Whitelist {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract Admin is Ownable {
     
-    address owner;
-
     enum addressStatus { Default, Blacklist, Whitelist }
 
-    event Defaulted(address _address);
     event Whitelisted(address _address);
     event Blacklisted(address _address);
-    
-    constructor () public {
-        owner = msg.sender;
-    }
 
     mapping (address => addressStatus) public list;
-    
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Caller is not the owner");
-        _;
+
+    function isWhitelisted(address _address) public view returns (bool) {
+        return list[_address] == addressStatus.Whitelist;
     }
 
+    function isBlacklisted(address _address) public view returns (bool) {
+        return list[_address] == addressStatus.Blacklist;
+    }
+    
     function addToWhitelist(address _address) public onlyOwner {
         require(list[_address] != addressStatus.Whitelist, "Address already whitelisted");
         list[_address] = addressStatus.Whitelist;
@@ -33,24 +31,4 @@ contract Whitelist {
         list[_address] = addressStatus.Blacklist;
         emit Blacklisted(_address);
     }
-
-    function addToDefault(address _address) public onlyOwner {
-        require(list[_address] != addressStatus.Default, "Address already default");
-        list[_address] = addressStatus.Default;
-        emit Defaulted(_address);
-    }
-
-/*    struct Person {
-        string name;
-        uint age;
-    }
-    Person[] public persons;
-    function addPerson(string memory _name, uint _age) public {
-        persons.push(Person(_name, _age));
-    }
-    function removePerson() public {
-        persons.pop();        
-    }
-*/
-    
 }
