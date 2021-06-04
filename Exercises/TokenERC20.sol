3b2994e0a6815ae6690d69d3b30c00d36688b80f
Nicofru <n.fruneau@gmail.com>
2021-06-04 15:56:50 +0200

    up

--- Exercises/TokenERC20.sol
+++ Exercises/TokenERC20.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
  
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
+/*
+    Find contract here :
+    https://ropsten.etherscan.io/address/0x5d5c14f62b94f2d0c7667917aaa357f9560ae77f
+*/
 contract AlyraToken is ERC20 {
 
     address public owner;
